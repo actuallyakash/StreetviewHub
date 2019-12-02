a5015805d1cae8a069d18bf4a98ea69eb3c70213
Akash <akash_gupta@hotmail.com>
2019-12-03 01:08:02 +0530

    Optimized toast and styling

--- resources/views/layouts/modal.blade.php
+++ resources/views/layouts/modal.blade.php
@@ -53,17 +53,12 @@
     </div>
 </div>
 
-  <div class="toast" style="position: absolute; top: 20px; right: 0;">
-    <div class="toast-header">
-      <img src="..." class="rounded mr-2" alt="...">
-      <strong class="mr-auto">Bootstrap</strong>
-      <small>11 mins ago</small>
-      <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
+<div class="toast toast-success">
     <div class="toast-body">
-      Hello, world! This is a toast message.
+        <button type="button" class="close" data-dismiss="toast" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        😍 Awesome. Thankyou for sharing with the community.
     </div>
-  </div>
+</div>
 @endauth
