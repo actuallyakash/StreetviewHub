596bbfb2597fd596f527f2f59372aa6892cdff6f
Akash <akash_gupta@hotmail.com>
2019-12-09 00:40:25 +0530

    Added image links for pano

--- resources/views/layouts/feed.blade.php
+++ resources/views/layouts/feed.blade.php
@@ -13,9 +13,9 @@
                     <div class="eyeshot-image">
                         <a class="eyeshot-link" href="#">
                         <picture>
-                            <source srcset="https://via.placeholder.com/400x300" media="(-webkit-min-device-pixel-ratio: 1.5), (min--moz-device-pixel-ratio: 1.5), (-o-min-device-pixel-ratio: 3/2), (min-device-pixel-ratio: 1.5), (min-resolution: 1.5dppx)">
-                            <source srcset="https://via.placeholder.com/400x300">
-                            <img class="img-fluid" alt="" src="https://via.placeholder.com/400x300">
+                            <source srcset="https://maps.googleapis.com/maps/api/streetview?size=400x300&pano={{ $shot->pano_id }}&heading={{ $shot->pano_heading }}&pitch={{ $shot->pano_pitch }}&key=AIzaSyBD52XR31rIk-MaE6AKlj_pLYlKxeJGUBQ" media="(-webkit-min-device-pixel-ratio: 1.5), (min--moz-device-pixel-ratio: 1.5), (-o-min-device-pixel-ratio: 3/2), (min-device-pixel-ratio: 1.5), (min-resolution: 1.5dppx)">
+                            <source srcset="https://maps.googleapis.com/maps/api/streetview?size=400x300&pano={{ $shot->pano_id }}&heading={{ $shot->pano_heading }}&pitch={{ $shot->pano_pitch }}&key=AIzaSyBD52XR31rIk-MaE6AKlj_pLYlKxeJGUBQ">
+                            <img class="img-fluid" alt="" src="https://maps.googleapis.com/maps/api/streetview?size=400x300&pano={{ $shot->pano_id }}&heading={{ $shot->pano_heading }}&pitch={{ $shot->pano_pitch }}&key=AIzaSyBD52XR31rIk-MaE6AKlj_pLYlKxeJGUBQ">
                         </picture>
                         </a>
                     </div>
@@ -43,9 +43,9 @@
                     <div class="eyeshot-image">
                         <a class="eyeshot-link" href="#">
                         <picture>
-                            <source srcset="https://via.placeholder.com/400x300" media="(-webkit-min-device-pixel-ratio: 1.5), (min--moz-device-pixel-ratio: 1.5), (-o-min-device-pixel-ratio: 3/2), (min-device-pixel-ratio: 1.5), (min-resolution: 1.5dppx)">
-                            <source srcset="https://via.placeholder.com/400x300">
-                            <img alt="" src="https://via.placeholder.com/400x300">
+                            <source srcset="https://maps.googleapis.com/maps/api/streetview?size=400x300&pano={{ $shot->pano_id }}&heading={{ $shot->pano_heading }}&pitch={{ $shot->pano_pitch }}&key=AIzaSyBD52XR31rIk-MaE6AKlj_pLYlKxeJGUBQ" media="(-webkit-min-device-pixel-ratio: 1.5), (min--moz-device-pixel-ratio: 1.5), (-o-min-device-pixel-ratio: 3/2), (min-device-pixel-ratio: 1.5), (min-resolution: 1.5dppx)">
+                            <source srcset="https://maps.googleapis.com/maps/api/streetview?size=400x300&pano={{ $shot->pano_id }}&heading={{ $shot->pano_heading }}&pitch={{ $shot->pano_pitch }}&key=AIzaSyBD52XR31rIk-MaE6AKlj_pLYlKxeJGUBQ">
+                            <img alt="" src="https://maps.googleapis.com/maps/api/streetview?size=400x300&pano={{ $shot->pano_id }}&heading={{ $shot->pano_heading }}&pitch={{ $shot->pano_pitch }}&key=AIzaSyBD52XR31rIk-MaE6AKlj_pLYlKxeJGUBQ">
                         </picture>
                         </a>
                     </div>
@@ -56,9 +56,9 @@
                     <div class="eyeshot-image">
                         <a class="eyeshot-link" href="#">
                         <picture>
-                            <source srcset="https://via.placeholder.com/400x300" media="(-webkit-min-device-pixel-ratio: 1.5), (min--moz-device-pixel-ratio: 1.5), (-o-min-device-pixel-ratio: 3/2), (min-device-pixel-ratio: 1.5), (min-resolution: 1.5dppx)">
-                            <source srcset="https://via.placeholder.com/400x300">
-                            <img alt="" src="https://via.placeholder.com/400x300">
+                            <source srcset="https://maps.googleapis.com/maps/api/streetview?size=400x300&pano={{ $shot->pano_id }}&heading={{ $shot->pano_heading }}&pitch={{ $shot->pano_pitch }}&key=AIzaSyBD52XR31rIk-MaE6AKlj_pLYlKxeJGUBQ" media="(-webkit-min-device-pixel-ratio: 1.5), (min--moz-device-pixel-ratio: 1.5), (-o-min-device-pixel-ratio: 3/2), (min-device-pixel-ratio: 1.5), (min-resolution: 1.5dppx)">
+                            <source srcset="https://maps.googleapis.com/maps/api/streetview?size=400x300&pano={{ $shot->pano_id }}&heading={{ $shot->pano_heading }}&pitch={{ $shot->pano_pitch }}&key=AIzaSyBD52XR31rIk-MaE6AKlj_pLYlKxeJGUBQ">
+                            <img alt="" src="https://maps.googleapis.com/maps/api/streetview?size=400x300&pano={{ $shot->pano_id }}&heading={{ $shot->pano_heading }}&pitch={{ $shot->pano_pitch }}&key=AIzaSyBD52XR31rIk-MaE6AKlj_pLYlKxeJGUBQ">
                         </picture>
                         </a>
                     </div>
@@ -69,9 +69,9 @@
                     <div class="eyeshot-image">
                         <a class="eyeshot-link" href="#">
                         <picture>
-                            <source srcset="https://via.placeholder.com/400x300" media="(-webkit-min-device-pixel-ratio: 1.5), (min--moz-device-pixel-ratio: 1.5), (-o-min-device-pixel-ratio: 3/2), (min-device-pixel-ratio: 1.5), (min-resolution: 1.5dppx)">
-                            <source srcset="https://via.placeholder.com/400x300">
-                            <img alt="" src="https://via.placeholder.com/400x300">
+                            <source srcset="https://maps.googleapis.com/maps/api/streetview?size=400x300&pano={{ $shot->pano_id }}&heading={{ $shot->pano_heading }}&pitch={{ $shot->pano_pitch }}&key=AIzaSyBD52XR31rIk-MaE6AKlj_pLYlKxeJGUBQ" media="(-webkit-min-device-pixel-ratio: 1.5), (min--moz-device-pixel-ratio: 1.5), (-o-min-device-pixel-ratio: 3/2), (min-device-pixel-ratio: 1.5), (min-resolution: 1.5dppx)">
+                            <source srcset="https://maps.googleapis.com/maps/api/streetview?size=400x300&pano={{ $shot->pano_id }}&heading={{ $shot->pano_heading }}&pitch={{ $shot->pano_pitch }}&key=AIzaSyBD52XR31rIk-MaE6AKlj_pLYlKxeJGUBQ">
+                            <img alt="" src="https://maps.googleapis.com/maps/api/streetview?size=400x300&pano={{ $shot->pano_id }}&heading={{ $shot->pano_heading }}&pitch={{ $shot->pano_pitch }}&key=AIzaSyBD52XR31rIk-MaE6AKlj_pLYlKxeJGUBQ">
                         </picture>
                         </a>
                     </div>
