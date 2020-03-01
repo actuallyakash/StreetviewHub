d3cf7b62192f5a43b2f37879ba4ee2c240907260
Akash <akash_gupta@hotmail.com>
2020-03-01 20:02:47 +0530

    // TODO: offline thingy :trollface:

--- public/js/index.js
+++ public/js/index.js
@@ -491,11 +491,7 @@
     /* PWA */
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', function () {
-            navigator.serviceWorker.register('/sw.js').then(function (registration) {
-                console.log('ServiceWorker registration :', registration.scope);
-            }).catch(function (error) {
-                console.log('ServiceWorker registration failed:', error);
-            });
+            navigator.serviceWorker.register('/sw.js');
         });
     }
     
