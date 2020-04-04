dccf8dcb74cb387a8973d6fdf2c8a2fb8f909de3
Akash <akash_gupta@hotmail.com>
2020-04-05 00:28:48 +0530

    Removed Job Delay ⏲

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -121,7 +121,7 @@ class LocationController extends Controller
 
         // Queueing Jobs
         \App\Jobs\TweetEyeshot::dispatch( $location->first() )->delay( now()->addMinutes(10) ); // Tweet
-        \App\Jobs\FbPostEyeshot::dispatch( $location->first() )->delay (now()->addMinutes(10) ); // Publish
+        \App\Jobs\FbPostEyeshot::dispatch( $location->first() ); // Publish
         
         return 1;
     }
