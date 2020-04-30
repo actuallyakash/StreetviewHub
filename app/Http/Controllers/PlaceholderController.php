2cb11f09ae63578f8efce4baa8315725da290de9
Akash <akash_gupta@hotmail.com>
2020-05-01 01:20:01 +0530

    Initial Setup 📦

---
+++ app/Http/Controllers/PlaceholderController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PlaceholderController extends Controller
+{
+    public function test()
+    {
+        dd('reached');
+    }
+}
