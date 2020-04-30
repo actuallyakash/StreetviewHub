246d10011cb10177f42ce688ff7c44bc1254a654
Akash <akash_gupta@hotmail.com>
2020-05-01 01:20:01 +0530

    Initial Setup 📦

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -65,7 +65,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapApiRoutes()
     {
-        Route::prefix('api')
+        Route::prefix('shots')
              ->middleware('api')
              ->namespace($this->namespace)
              ->group(base_path('routes/api.php'));
