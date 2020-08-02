0210803f9a8166e0c911cbba12b17b381d7887b8
Akash Gupta <akash_gupta@hotmail.com>
2020-08-02 14:29:26 +0530

    Added Newsletter Design 💌

--- resources/views/components/newsletter.blade.php
+++ resources/views/components/newsletter.blade.php
@@ -1,18 +1,13 @@
-<div class="newsletter d-flex justify-content-center m-5">
-    <div class="row">
-        <div class="p-4 card text-center">
-            <h2>Newsletter</h2>
-
-            <p>Subscribe to our weekly Newsletter and stay tuned.</p>
-
-            <form action="" method="post" role="form">
-                <div class="input-group flex-nowrap">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text" id="addon-wrapping"><i class="fa fa-envelope"></i></span>
-                    </div>
-                    <input type="email" class="form-control" placeholder="Email" aria-label="Email" aria-describedby="addon-wrapping">
-                </div>
-                <input type="submit" value="Subscribe" class="btn btn-large btn-primary mt-2" />
+<div class="container">
+    <div class="newsletter m-5 p-5 d-flex align-items-center justify-content-center justify-content-xl-between flex-wrap text-center text-xl-left">
+        <div class="newsletter-content">
+            <h2>Each week I share new eyeshots <br>in the Newsletter.</h2>
+            <p class="sub-text text-white-50"> With Beautiful, cool and awesome locations. I'd love you to join. </p>
+        </div>
+        <div class="newsletter-cta">
+            <form action="" method="post" role="form" class="d-flex flex-wrap flex-md-nowrap">
+                <input class="email" type="email" placeholder="Your email">
+                <button type="submit" class="btn btn-large ml-md-3 col-md-4 col-12 col-form-lable-lg font-weight-bolder ">Try it out</button>
             </form>
         </div>
     </div>
